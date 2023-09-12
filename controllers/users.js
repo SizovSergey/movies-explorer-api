3c591c5304c3e77333471bbb7ef2c01821bba9d9
SizovSergey <118894697+SizovSergey@users.noreply.github.com>
2023-09-12 07:49:10 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -52,29 +52,24 @@ module.exports.getCurrentUser = (req, res, next) => {
     .catch((err) => next(err));
 };
 
-module.exports.updateUser = async (req, res, next) => {
-  try {
-    const { name, email } = req.body;
-    const currentUser = await User.findOne({ email });
+module.exports.updateUser = (req, res, next) => {
+  const { name, email } = req.body;
 
-    if (currentUser && currentUser._id !== req.user._id) {
-      throw new ConflictError('Введите другой email. Пользователь с таким email уже существует');
-    }
-
-    const user = await User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true });
-
-    if (!user) {
-      throw new NotFoundError({ message: 'Нет пользователя с таким id' });
-    }
-
-    res.status(200).send(user);
-  } catch (err) {
-    if (err.name === 'CastError') {
-      return next(new BadRequestError('Неправильный тип данных'));
-    }
-    return next(err);
-  }
+  User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError({ message: 'Нет пользователя с таким id' });
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('Неправильный тип данных'));
+      }
+      return next(err);
+    });
 };
+
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
